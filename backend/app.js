d23adea05552936723b31181f94cbb0f3f778180
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-10-28 17:51:03 -0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
 
 // Iniciando o App
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 // Iniciando o DB
 mongoose.connect('mongodb://db/nodeapi', { useNewUrlParser: true} )
 requireDir('./src/models')
-
-// const Product = mongoose.model('Product')
 
 // Rotas
 app.use('/api', require('./src/routes'))
