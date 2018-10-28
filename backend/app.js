36dddaa780456a44ca74e64de749c86d4f2ab853
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-10-28 17:17:10 -0300

    create register

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const requireDir = require('require-dir')
 
 // Iniciando o App
 const app = express()
+app.use(express.json())
 
 // Iniciando o DB
 mongoose.connect('mongodb://db/nodeapi', { useNewUrlParser: true} )
