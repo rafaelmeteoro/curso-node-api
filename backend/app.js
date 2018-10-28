d51c4c353b14a5382d17d49de68a375b0d9d0a1c
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-10-28 14:23:33 -0300

    restructure files

--- backend/app.js
+++ backend/app.js
@@ -9,17 +9,9 @@ const app = express()
 mongoose.connect('mongodb://db/nodeapi', { useNewUrlParser: true} )
 requireDir('./src/models')
 
-const Product = mongoose.model('Product')
+// const Product = mongoose.model('Product')
 
-// Primeira rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native app with React',
-        url: 'http://github.com/facebook/react-native'
-    })
-
-    return res.send('Hello World 2')
-})
+// Rotas
+app.use('/api', require('./src/routes'))
 
 app.listen(3000)
